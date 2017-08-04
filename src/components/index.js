bac68075811189b5db063d0833d080c3c3f9b348
Emmanuel Jucaban <EmmanuelJucaban@gmail.com>
2017-08-03 20:43:41 -0700

    Init repo from boilerplate

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class AppIndex extends Component {
+  render() {
+    return (<div>React, bootstrap, redux, and webpack 3 boilerplate</div>);
+  }
+}
+
+export default AppIndex;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import promise from 'redux-promise';
+
+import AppIndex from 'components';
+import reducers from 'reducers';
+
+const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+
+
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <div>
+      <Router>
+        <div>
+          <Route path="/" component={AppIndex}></Route>
+        </div>
+      </Router>
+    </div>
+  </Provider>
+  , document.querySelector('.container'));
